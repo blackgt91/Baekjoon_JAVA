364bff2f1fba11089723388f644c998413ecd75b
blackgt91 <59950949+blackgt91@users.noreply.github.com>
2020-06-25 01:44:29 +0900

    Update test_1408.java

--- test_1408.java
+++ test_1408.java
@@ -10,15 +10,15 @@ public class test_1408 {
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = new StringTokenizer(br.readLine(),":");
     
-    // 시간 데이터 형식을 초(Integer, 0~24*3600-1) 단위 형태로 변환하여 초기화 
-    // 끝난 시간 - 시작 시간
+    		// 시간 데이터 형식을 초(Integer, 0~24*3600-1) 단위 형태로 변환하여 초기화 
+    		// 끝난 시간 - 시작 시간
 		int seconds = -(3600*Integer.parseInt(st.nextToken()) + 60*Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())); 
 		st = new StringTokenizer(br.readLine(),":");
 		seconds += 3600*Integer.parseInt(st.nextToken()) + 60*Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken());
 		
 		if(seconds < 0) seconds += 3600*24; // 자정을 지난 경우(0초부터 다시 시작한 경우) 예외처리
     
-    // 시간 단위별 출력
+    		// 시간 단위별 출력
 		int hour = seconds / 3600; 
 		seconds %= 3600;
 		int minute = seconds / 60;
