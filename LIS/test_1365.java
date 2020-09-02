1acc83d1604424acb9cb592da0786abcbc03289e
blackgt91 <59950949+blackgt91@users.noreply.github.com>
2020-09-02 19:57:01 +0900

    Update and rename LIS_lowerBound.java to test_1365.java

--- LIS/test_1365.java
+++ LIS/test_1365.java
@@ -1,8 +1,9 @@
+// Problem Link : https://www.acmicpc.net/problem/1365
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class LIS_lowerBound {
+public class test_1365 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
